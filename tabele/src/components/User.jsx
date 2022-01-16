97e72012bbf76cbe4effbce216e8c7d0af59cc84
Jana Jolovic <janajolovic04@gmail.com>
2022-01-16 17:27:01 +0100

    user info btn

--- tabele/src/components/User.jsx
+++ tabele/src/components/User.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const User = ({ usr, rmUser, editUser}) => {
   return (
@@ -19,6 +20,7 @@ const User = ({ usr, rmUser, editUser}) => {
           editUser(usr.id)
         }}>
           Edit</button>
+        <Link to={`/${usr.id}`}> <button>User Info</button></Link>
       </td>
     </tr>
   );
